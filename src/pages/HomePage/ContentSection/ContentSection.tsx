ba0bf140661ad273aadb1b0c541beb0ac27d5068
YaoHui <yao.hui.yh.1995@gmail.com>
2026-08-01 15:18:16 +0800

    Hotfix: amended content and images

--- src/pages/HomePage/ContentSection/ContentSection.tsx
+++ src/pages/HomePage/ContentSection/ContentSection.tsx
@@ -6,14 +6,15 @@ import { useNavigate } from 'react-router-dom'
 import ContentCard from '../ContentCard/ContentCard'
 import CountdownTimer from '../../../components/CountDownTimer'
 /* ── import context / hooks ─────────────────────────────────────── */
-import { useImageModal } from '../../../components/ImageModalContext'
+// import { useImageModal } from '../../../components/ImageModalContext'
 /* ── import assets ──────────────────────────────────────────────── */
 import leftTop from "../../../assets/Corner Flower Top (2).png"
 import leftBottom from "../../../assets/Corner Flower Bottom (2).png"
 import image1 from "../../../assets/shoes.png"
-import poloroid1 from "../../../assets/image (3).png"
-import poloroid2 from "../../../assets/image (4).png"
-import poloroid3 from "../../../assets/image (5).png"
+import image2 from "../../../assets/DSC_2800.jpg"
+// import poloroid1 from "../../../assets/image (3).png"
+// import poloroid2 from "../../../assets/image (4).png"
+// import poloroid3 from "../../../assets/image (5).png"
 import LocationMap from '../../../components/LocationMap'
 
 
@@ -26,7 +27,7 @@ type ContentSectionProp = {
 export default function ContentSection({ eventTime }: ContentSectionProp) {
   /* ── Hooks ────────────────────────────────────────────────────── */
   const navigate = useNavigate();
-  const { openImage } = useImageModal();
+  // const { openImage } = useImageModal();
 
   /* ── Render ───────────────────────────────────────────────────── */
   return (
@@ -37,24 +38,30 @@ export default function ContentSection({ eventTime }: ContentSectionProp) {
         <img src={leftBottom} className="corner bottom-right"/>
         <h1 className="date-header">{eventTime.toLocaleDateString("en-SG", { weekday: 'long' })}<br/>{eventTime.toLocaleDateString("en-SG", { year: 'numeric', month: 'long', day: 'numeric' })}</h1>
         <p className="address-header">Vineyard @ Hortpark</p>
-        <button className="primary-button" onClick={() => navigate('/rsvp')}>RSVP</button>
+        <button className="primary-button" onClick={() => navigate('/rsvp')}>Menu Selection</button>
       </div>
     </ContentCard>
     <ContentCard type='photo'>
       <img
-          src="https://images.unsplash.com/photo-1522673607200-164d1b6ce486?auto=format&fit=crop&w=1200&q=80"
-          alt="Wedding photo 1"
-          className="wedding-photo"
-        />
+        src={image2}
+        alt="Wedding photo 1"
+        className="wedding-photo"
+      />
     </ContentCard>
     <ContentCard>
       <CountdownTimer targetDate={eventTime}/>
     </ContentCard>
     <ContentCard>
       <p className="address-header">OUR STORY</p>
-      <p className="text-header" style={{ textAlign: "justify" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus neque in nibh iaculis interdum. Ut congue ac turpis et tincidunt. Quisque placerat mattis tortor a eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium elit nisi, vitae maximus ipsum interdum eget. Vestibulum at ornare felis. Fusce accumsan magna fermentum feugiat tempus. Integer tristique pellentesque dapibus. Curabitur sit amet sem commodo, sagittis nunc ac, interdum lacus. Nulla vel elit sollicitudin, molestie sapien at, aliquam erat. Etiam non lorem non dolor accumsan sagittis feugiat et magna.</p>
+      <p className="text-header" style={{ textAlign: "justify" }}>Our story began with a lucky swipe right, which quickly bloomed into a love that changed our lives forever. Over the years, we have been completely inseparable, finding joy in everything from getting creative with handicrafts and cooking together to exploring different parts of Asia. </p>
+      <p className="text-header" style={{ textAlign: "justify" }}>Our engagement on 12 September 2025 was the perfect reflection of the deep bond we share. We are incredibly excited for this next chapter and can’t wait to celebrate our wedding day alongside our cherished family and friends!</p>
     </ContentCard>
     <ContentCard>
+      <p className="address-header">我们的故事</p>
+      <p className="text-header" style={{ textAlign: "justify" }}>我们的故事，始于一次美好的缘分。一次相遇，让两个原本陌生的人走进了彼此的生命，也开启了属于我们的爱情旅程。这些年来，我们一起经历了许多难忘的时光。从动手做手工、一起下厨，到携手探索亚洲各地，每一次欢笑、每一段旅程，都让我们的感情更加深厚，也留下了无数珍贵的回忆。</p>
+      <p className="text-header" style={{ textAlign: "justify" }}>2025年9月12日的求婚，更是见证了我们深厚而坚定的感情，也为我们的爱情开启了崭新的篇章。如今，我们满怀期待地迎接人生的下一段旅程，并迫不及待地与挚爱的家人和朋友们，一同庆祝属于我们的婚礼，分享这份喜悦与幸福。</p>
+    </ContentCard>
+    {/* <ContentCard>
       <img src={poloroid1} style={{ marginBottom: 50, position: "sticky", top: "15%" }} onClick={()=>openImage?.(poloroid1)}/>
       <img src={poloroid2} style={{ marginBottom: 50, position: "sticky", top: "15%" }} onClick={()=>openImage?.(poloroid2)}/>
       <img src={poloroid3} style={{ marginBottom: 50, position: "sticky", top: "15%" }} onClick={()=>openImage?.(poloroid3)}/>
@@ -62,7 +69,7 @@ export default function ContentSection({ eventTime }: ContentSectionProp) {
     <ContentCard>
       <p className="address-header">TIDBIT</p>
       <p className="text-header" style={{ textAlign: "justify" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus neque in nibh iaculis interdum. Ut congue ac turpis et tincidunt. Quisque placerat mattis tortor a eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium elit nisi, vitae maximus ipsum interdum eget. Vestibulum at ornare felis. Fusce accumsan magna fermentum feugiat tempus. Integer tristique pellentesque dapibus. Curabitur sit amet sem commodo, sagittis nunc ac, interdum lacus. Nulla vel elit sollicitudin, molestie sapien at, aliquam erat. Etiam non lorem non dolor accumsan sagittis feugiat et magna.</p>
-    </ContentCard>
+    </ContentCard> */}
     <ContentCard type='photo'>
       <img
         src={image1}
@@ -72,21 +79,22 @@ export default function ContentSection({ eventTime }: ContentSectionProp) {
     </ContentCard>
     <ContentCard>
       <p className="address-header">Getting to Vineyard @ Hortpark</p>
-      <p className="text-header" style={{ textAlign: "justify" }}>*You are recommended to come earlier to enjoy a scenic stroll through the park*</p>
+      <p className="text-header" style={{ textAlign: "center" }}>*You are recommended to come earlier to enjoy a scenic stroll through the park*</p>
       <p className="text-header" style={{ textAlign: "center" }}><strong>🚇 By MRT & Bus</strong></p>
       <p className="text-header" style={{ textAlign: "justify" }}><strong>A. Labrador Park MRT (CC27):</strong> Take Exit A. Walk through the sheltered walk way to Alexandra Retail Ctr bus stop. Board Bus 51, 61, 93, 97, 100, 120, 166 or 963. Alight 2 stops later at Aft HortPark stop on Alexandra Road. Walk 5 minutes down Hyderabad Road to the park entrance.</p>
       <p className="text-header" style={{ textAlign: "justify" }}><strong>B. Queenstown MRT (EW19):</strong> Take Exit A/D. Walk to the bus stop right outside the Queenstown Stn Ext A/D.  Board Bus 51 or 111 and alight at Bef Telok Blangah Hill Park along Alexandra Road. Cross the road via overhead bridge to Hortpark side and walk down Hyderabad Road into Hortpark.</p>
       <p className="text-header" style={{ textAlign: "justify" }}><strong>C. HarbourFront MRT (NE1/CC29):</strong> Go to the bus interchange. Board Bus 61, 100, or 166 and alight at Aft Hortpark stop.</p>
       <p className="text-header" style={{ textAlign: "center" }}><strong>🚌 By Direct Bus</strong></p>
-      <p className="text-header" style={{ textAlign: "justify" }}>You can take any of these buses directly to Alexandra Road, alighting at either the Aft Hortpark stop or Bef Telok Blangah Hill.</p>
-      <p className="text-header" style={{ textAlign: "justify" }}>Bus numbers: 51, 61, 93, 97, 100, 120, 166, 963.</p>
+      <p className="text-header" style={{ textAlign: "justify" }}>Take any of these buses directly to Alexandra Road, alighting at either the Aft Hortpark stop or Bef Telok Blangah Hill.</p>
+      <p className="text-header" style={{ textAlign: "justify" }}><strong>Bus numbers:</strong> 51, 61, 93, 97, 100, 120, 166, 963.</p>
       <p className="text-header" style={{ textAlign: "center" }}><strong>🚌 By Car</strong></p>
       <p className="text-header" style={{ textAlign: "justify" }}>Access the park via Alexandra Road and turn into Hyderabad Road.</p>
-      <p className="text-header" style={{ textAlign: "justify" }}>Parking: The park has an on-site paid parking lot ($0.60 per 30 mins or $0.02 per min) with 149 car lots and accessible parking spaces.</p>
+      <p className="text-header" style={{ textAlign: "justify" }}><strong>Parking:</strong> The park has an on-site paid parking lot ($0.60 per 30 mins or $0.02 per min) with 149 car lots and accessible parking spaces.</p>
       <LocationMap address="Vineyard @ Hortpark, Singapore"/>
     </ContentCard>
     <ContentCard>
-      <p className="address-header">Notes to all guests (Please read carefully!)</p>
+      <p className="address-header">Notes to all guests</p>
+      <p className="text-header">Please read carefully!</p>
       <p className="text-header" style={{ textAlign: "justify" }}>Due to the wedding venue (Vineyard @ Hortpark) being subjected to restrictions and stipulations imposed by the National Parks Board (NParks):</p>
       <ul>
         <li><p className="text-header" style={{ textAlign: "justify" }}>No scattering of flower petals or confetti is allowed within Hortpark (a cleaning fee of $500++ will be imposed if otherwise)</p></li>
@@ -96,7 +104,8 @@ export default function ContentSection({ eventTime }: ContentSectionProp) {
       </ul>
     </ContentCard>
     <ContentCard>
-      <p className="address-header">致所有宾客（请仔细阅读）</p>
+      <p className="address-header">致所有宾客</p>
+      <p className="text-header">请仔细阅读！</p>
       <p className="text-header" style={{ textAlign: "justify" }}>由于婚礼场地 (Vineyard @ Hortpark) 受国家公园局（NParks）的限制和规定约束，特此说明：</p>
       <ul>
         <li><p className="text-header" style={{ textAlign: "justify" }}>园艺园内禁止撒花瓣或彩纸屑（如有违反，将收取500新元++的清洁费）。</p></li>
@@ -105,13 +114,13 @@ export default function ContentSection({ eventTime }: ContentSectionProp) {
         <li><p className="text-header" style={{ textAlign: "justify" }}>园艺园及餐厅的宴会厅内禁止使用明火（包括烟花、打火机、蜡烛等）。</p></li>
       </ul>
     </ContentCard>
-    <ContentCard type='photo'>
+    {/* <ContentCard type='photo'>
         <img
           src="https://images.unsplash.com/photo-1492724441997-5dc865305da7?auto=format&fit=crop&w=1200&q=80"
           alt="Wedding photo 3"
           className="wedding-photo"
         />
-    </ContentCard>
+    </ContentCard> */}
     </>
   )
 }
